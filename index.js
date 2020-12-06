9b1edcad4c5c0d589db1834a412e2460b2791284
ktomkow <krzysztof.tomkow@gmail.com>
2020-12-06 20:50:52 +0100

    Decode address in base64

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-const { default: Axios } = require('axios')
+const { default: Axios } = require('axios');
 const axios = require('axios');
-const express = require('express')
-const app = express()
-const port = 4999
+const express = require('express');
+const app = express();
+const port = 4999;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-app.get('/catalog', (req,res) => {
-  axios.get('http://192.168.0.133:9997/v2/_catalog/').then(response => {
-    res.send(response.data);
+app.get('/passOn/:addressInBase64', (req, res) => {
+  const addressInBase64 = req.params.addressInBase64;
+  let buff = new Buffer.from(addressInBase64, 'base64');
+  const address = buff.toString('ascii');
+
+  axios.get('http://192.168.0.133:9997/v2/_catalog/') // change this address
+    .then((response) => {
+      // res.send(response.data);
+      res.send(address);
   });
-  
-})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
