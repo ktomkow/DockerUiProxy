0cf5c7c743a5cb5e67afb91d76f121bb0ee39c6c
ktomkow <krzysztof.tomkow@gmail.com>
2020-12-06 20:27:18 +0100

    First endpoint to check if can access docker local registry

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+const { default: Axios } = require('axios')
+const axios = require('axios');
 const express = require('express')
 const app = express()
 const port = 4999
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/catalog', (req,res) => {
+  axios.get('http://192.168.0.133:9997/v2/_catalog/').then(response => {
+    res.send(response.data);
+  });
+  
 })
 
 app.listen(port, () => {
